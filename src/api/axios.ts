83b4c9250e9efc655fbcdd380da9230d4fbf1eb2
mikhail_rypta <senseika@gmail.com>
2022-07-28 19:10:10 +0300

    remove unnecessary files

---
+++ src/api/axios.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const api = axios.create({
+	baseURL: "https://pokeapi.co/api/v2/",
+});
+
+export interface AxiosResponse<T> {
+	count: number;
+	next: string;
+	previous: string;
+	results: T[];
+}
---
+++ src/api/pokemon.ts
@@ -0,0 +1,21 @@
+import { api, AxiosResponse } from "./axios";
+
+import { Pokemon, Pokemons } from "./../models/pokemon";
+
+export const getPokemons = async () => {
+	const { data } = await api.request<AxiosResponse<Pokemons[]>>({
+		method: "GET",
+		url: "pokemon",
+	});
+
+	return data;
+};
+
+export const getPokemon = async (name: string) => {
+	const { data } = await api.request<AxiosResponse<Pokemon[]>>({
+		method: "GET",
+		url: `pokemon/${name}`,
+	});
+
+	return data;
+};
